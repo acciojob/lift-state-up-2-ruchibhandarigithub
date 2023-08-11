376840e3011f4df2da86ddadd4f01ec6597c2f10
unknown <bhandariruchi498@gmail.com>
2023-08-11 19:38:16 +0530

    day

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React ,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText] = useState("");
+  const changeText = (e)=>{
+    setText(e.target.value);
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <p>{text}</p>
+      <div className="child">
+        <h2>Child Component</h2>
+        <input type="text" onChange={changeText} />
+      </div>
+       
     </div>
   )
 }
